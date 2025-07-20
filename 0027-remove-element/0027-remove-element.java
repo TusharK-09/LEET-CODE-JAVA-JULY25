5da184b024d143f22d5bfd00fa5190033cb15aef
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-07-20 22:06:38 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.93%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,12 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int ptr = 0;
+        int k = 0;
         for(int i=0; i<nums.length; i++){
             if(nums[i] != val){
-                nums[ptr] = nums[i];
-                ptr++;
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return ptr;
+        return k;
     }
 }
