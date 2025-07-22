d74345daca84371aa82314219a831af27fb31038
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-07-22 11:26:09 +0530

    Time: 0 ms (100%), Space: 41.6 MB (43.11%) - LeetHub

--- 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.java
+++ 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.java
@@ -1,41 +1,30 @@
 class Solution {
-    public int dominantIndex(int[] nums) {
 
+    public int dominantIndex(int[] nums) {
         //Finding largest
         int max = nums[0];
         int maxIndex = 0;
         int secondLargest = nums[0];
-        
-        for(int i=0; i<nums.length; i++){
-            
-            if(nums[i] >= max){
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= max) {
                 max = nums[i];
 
                 maxIndex = i;
-
             }
-
         }
 
-        //compare max to twice of evry elemnt expect itself
-        for(int i=0; i<nums.length; i++){
-
-           if(i == maxIndex){
-
-            continue;
-
-           }
-
-           else {
-
-               if (max < 2 * nums[i]) {
+        //Compare max to twice of every element of nums expect itself index
+        for (int i = 0; i < nums.length; i++) {
+            if (i == maxIndex) {
+                continue;
+            } else {
+                if (max < 2 * nums[i]) {
                     return -1;
-                }  
-           }
-         
+                }
+            }
         }
-       
-     return maxIndex;
+
+        return maxIndex;
     }
 }
