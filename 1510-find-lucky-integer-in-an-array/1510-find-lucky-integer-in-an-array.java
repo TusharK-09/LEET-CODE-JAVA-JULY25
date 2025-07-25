44981ad19dd44dc69990cb66dc45038a0620d15d
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-07-25 19:41:59 +0530

    Time: 5 ms (61.5%), Space: 43.4 MB (47.56%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -19,7 +19,7 @@ class Solution {
                 frequency =  e.getValue();
 
                 if(number == frequency){
-                    luckyNumber = number;
+                    luckyNumber = Math.max(luckyNumber , number);
                 }
 
         }
