b3ab48eacf2378ece63cf797ab95e7669441e54d
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-08-01 02:12:05 +0530

    Time: 14 ms (31.83%), Space: 45.1 MB (24.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // 1. Convert to lowercase FIRST.
-        // 2. Then, remove all non-alphanumeric characters.
+       
         String filtered = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-        // 3. Create the reversed version.
+     
         String reversed = new StringBuilder(filtered).reverse().toString();
 
-        // 4. Compare them.
         return filtered.equals(reversed);
     }
 }
