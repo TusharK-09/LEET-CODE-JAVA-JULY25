e3d471c86c0f6e2e286e66cb32b5261b2d592a30
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-08-02 22:50:58 +0530

    Time: 3 ms (38.04%), Space: 42.3 MB (6.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,6 +4,7 @@ class Solution {
         if (s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
+            
             if (stack.isEmpty() &&(s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']')
             ) {
                 return false;
@@ -13,7 +14,7 @@ class Solution {
                     if (s.charAt(i) == ')' && stack.peek() == '(') stack.pop(); 
                     else if (s.charAt(i) == '}' && stack.peek() == '{') stack.pop();
                     else if (s.charAt(i) == ']' && stack.peek() == '[') stack.pop(); 
-                    
+
                     else stack.add(s.charAt(i));
             }
         }
