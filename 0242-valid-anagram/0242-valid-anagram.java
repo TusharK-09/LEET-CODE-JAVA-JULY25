9f6f301f404562489db98b77f6659da5b6de0f52
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-08-08 17:50:21 +0530

    Time: 4 ms (77.31%), Space: 44.8 MB (34.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,15 +3,17 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character , Integer> sMap = new HashMap<>();
-        HashMap<Character , Integer> tMap = new HashMap<>();
 
-        for(int i=0; i<s.length(); i++){
-            sMap.put(s.charAt(i) , sMap.getOrDefault(s.charAt(i) , 0) + 1);
-            tMap.put(t.charAt(i) , tMap.getOrDefault(t.charAt(i) , 0) + 1);
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+
+        if(Arrays.equals(sChar , tChar)){
+            return true;
         }
 
-        return sMap.equals(tMap);
-
+        return false;
     }
 }
