2e8b4df330554052bccf69bd8aba1798c3f60de6
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-08-18 09:37:05 +0530

    Time: 1 ms (99.52%), Space: 57.2 MB (42.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -6,12 +6,15 @@ class Solution {
         int sum =0;
 
         for(int i=0; i<nums.length; i++){
+            //adding every element to the sum
             sum = sum + nums[i];
 
+            //updating maximum sum condition
             if(sum > maximum){
                 maximum = sum;
             }
 
+            //if sum is < 0 i.e negative we dont carry any negative sum
             if(sum < 0){
                 sum = 0;
             }
