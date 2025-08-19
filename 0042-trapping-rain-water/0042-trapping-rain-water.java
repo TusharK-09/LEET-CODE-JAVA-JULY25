a5f58f4d6e417bd7c1d343fc0e6a5a58b62067dc
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-08-19 10:31:39 +0530

    Time: 0 ms (100%), Space: 46.8 MB (20%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int trap(int[] height) {
+      //keeing track of left and right greatest elements
+      int leftMax = 0;
+      int rightMax = 0;
+      int water = 0;
+      
+      int left = 0;
+      int right = height.length - 1;
+
+      while(left < right){
+        if(height[left] < height[right]){
+            
+            if(height[left] > leftMax){
+                leftMax = height[left];
+            }
+            else{
+            water = water + leftMax - height[left];
+            }
+            left++;
+        }
+
+        else{
+            if(height[right] > rightMax){
+                rightMax = height[right];
+            }
+            else{
+                water = water + rightMax - height[right];
+            }
+            right--;
+        }
+      }
+
+      return water;
+
+      //  ============ Gives TLE for Large inputs =====================
+    //   //one loop to iterate over array
+    //   for(int i=0; i<height.length; i++){
+
+    //     //Get left max
+    //     leftMax = height[i];
+    //     for(int j=0; j<i; j++){
+    //         if(height[j] >  leftMax){
+    //             leftMax = height[j];
+    //         }
+    //     }
+
+    //     //Get right max
+    //     rightMax = height[i];
+    //     for(int k=i; k<height.length; k++){
+    //         if(height[k] > rightMax){
+    //             rightMax = height[k];
+    //         }
+    //     }
+
+        //Water stored
+       // water += Math.max( 0 , Math.min (leftMax , rightMax) - height[i] );
+     // return water;
+// ================================   ==========================================
+    }
+}
