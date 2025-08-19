7724f21fb37589c03c6d0238ec365f7efc5539e2
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-08-20 01:55:52 +0530

    Time: 1 ms (46.04%), Space: 57.4 MB (67.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,16 +1,20 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int [] ans = new int[nums.length];
         k = k % nums.length;
-        int ansPtr = 0;
-        for(int i= nums.length - k; i < nums.length; i++){
+        int [] ans = new int[nums.length];
+        int ansPtr  = 0;
+
+        //getting k stepped elements at start
+        for(int i=nums.length - k; i<nums.length; i++){
             ans[ansPtr++] = nums[i];
         }
 
-        for(int i=0; i< nums.length - k; i++){
+        //remaining to end as empty spaces firstly shifted elements
+        for(int i=0; i<nums.length - k; i++){
             ans[ansPtr++] = nums[i];
         }
 
+        //copy replace answer
         for(int i=0; i<nums.length; i++){
             nums[i] = ans[i];
         }
