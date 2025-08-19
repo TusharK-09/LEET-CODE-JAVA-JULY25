011ef94fb1dc867db7dcf1515105232d119a1bdf
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-08-19 14:48:00 +0530

    Time: 1 ms (99.52%), Space: 56.8 MB (89.59%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,25 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //kadane's algo
+      int  max =  Integer.MIN_VALUE;
+      int sum = 0;
+      for(int i=0; i<nums.length; i++){
+        sum = sum + nums[i];
 
-        int maximum = Integer.MIN_VALUE;
-        int sum =0;
-
-        for(int i=0; i<nums.length; i++){
-            //adding every element to the sum
-            sum = sum + nums[i];
-
-            //updating maximum sum condition
-            if(sum > maximum){
-                maximum = sum;
-            }
-
-            //if sum is < 0 i.e negative we dont carry any negative sum
-            if(sum < 0){
-                sum = 0;
-            }
+        if(sum > max){
+            max = sum;
         }
 
-        return maximum;
+        if(sum < 0){
+            sum = 0;
+        }
+      }  
+      return max;
     }
 }
