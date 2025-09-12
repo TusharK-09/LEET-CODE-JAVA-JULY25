2230f6e7b67bcbe21cd17f34338d340eb5f9a959
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-09-13 01:14:05 +0530

    Time: 3 ms (61.67%), Space: 45 MB (47.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //o(n) using HashMap
         HashMap<Integer , Integer> map = new HashMap<>();
 
         for(int i=0; i<nums.length; i++){
             int needed = target - nums[i];
 
-            //check if needed element already exist in map
-            if( map.containsKey(needed) ){
+            if(map.containsKey(needed)){
                 return new int[] {map.get(needed) , i};
             }
 
-            //keep inserting if needed does not exist
             map.put(nums[i] , i);
         }
-        return new int[] {-1,-1};
+
+        return new int[] {-1 ,  -1};
     }
 }
