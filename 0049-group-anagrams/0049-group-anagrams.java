9515b8eefa476142ce8894147900c4e3b2d92b06
Tushar Koundal <154369254+TusharK-09@users.noreply.github.com>
2025-10-10 08:30:13 +0530

    Time: 6 ms (98.67%), Space: 48 MB (45.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,26 +2,15 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String , List<String>> anagramGroups = new HashMap<>();
 
-        for(String word : strs){
-            int[] charsCount = new int[26];
+        for(String word  : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
 
-            //lets make array for a word
-            for(char ch : word.toCharArray()){
-                charsCount[ch - 'a']++;
-            }
-
-            //lets build unique key for a word by adding the redundation or keeping it unique
-            StringBuilder freqKey = new StringBuilder();
-            for(int i=0; i<26; i++){
-                freqKey.append("#").append(charsCount[i]);
-            }
-
-            //convert freqKey back to string type
-            String signature = freqKey.toString();
-
-            anagramGroups.putIfAbsent(signature , new ArrayList<>());
-            anagramGroups.get(signature).add(word);
+            String sortedKey = new String (chars);
+            anagramGroups.putIfAbsent(sortedKey , new ArrayList<>());
+            anagramGroups.get(sortedKey).add(word);
         }
+
         return new ArrayList<>(anagramGroups.values());
     }
 }
